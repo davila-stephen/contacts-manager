bcba068c9e4ece7d0ab1d53a634ccabc63de7cb4
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-11 11:58:44 -0500

    Feat:added option 1

--- src/ContactManager/contactsClass.java
+++ src/ContactManager/contactsClass.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class contactsClass {
 
+    // View contacts method
     public static void printContacts(Path filePath) throws IOException {
         System.out.println();
         List<String> fileContents = Files.readAllLines(filePath);
@@ -19,15 +20,55 @@ public class contactsClass {
         }
     }
 
+    // Add a new contact
+
 
     public static void main(String[] args) throws IOException {
         Path PathToContact = Paths.get("./src/ContactManager/contacts.txt");
 
         System.out.println("Files.exists(PathToContact) = " + Files.exists(PathToContact));
 
-        System.out.println("Name    | Phone Number");
-        System.out.println("----------------------");
-        printContacts(PathToContact);
+//        System.out.println("Name    | Phone Number");
+//        System.out.println("----------------------");
+//        printContacts(PathToContact);
+
+
+        // Main Menue
+        boolean looper = true;
+        while (looper) {
+            System.out.println("Here are some more options to move forward:");
+            System.out.println("1 - View contacts.");
+            System.out.println("2 - Add a new contact.");
+            System.out.println("3 - Search a contact by name.");
+            System.out.println("4 - Delete an existing contact.");
+            System.out.println("5 - Exit");
+            int yOrN = sc.nextInt();
+            if (yOrN == 1) {
+                System.out.println("Name    | Phone Number");
+                System.out.println("----------------------");
+                printContacts(PathToContact);
+
+                looper = false;
+            }
+            else if (yOrN == 2) {
+
+            }
+            else if(yOrN == 3) {
+
+            }
+            else if (yOrN == 4) {
+
+            }
+            else if (yOrN == 5) {
+                looper = false;
+            }
+            else {
+                System.out.println("Try again.");
+            }
+        }
+
 
     }
+
+
 }
