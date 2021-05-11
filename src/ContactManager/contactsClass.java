674e58f41b295f46aa13664ee1d086c798819d67
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-11 10:55:33 -0500

    feat: got contacts to print out on terminal

--- src/ContactManager/contactsClass.java
+++ src/ContactManager/contactsClass.java
@@ -11,5 +11,23 @@ import java.util.List;
 
 public class contactsClass {
 
+    public static void printContacts(Path filePath) throws IOException {
+        System.out.println();
+        List<String> fileContents = Files.readAllLines(filePath);
+        for (int i = 0; i < fileContents.size(); i++) {
+            System.out.printf("%s\n", fileContents.get(i));
+        }
+    }
 
+
+    public static void main(String[] args) throws IOException {
+        Path PathToContact = Paths.get("./src/ContactManager/contacts.txt");
+
+        System.out.println("Files.exists(PathToContact) = " + Files.exists(PathToContact));
+
+        System.out.println("Name    | Phone Number");
+        System.out.println("----------------------");
+        printContacts(PathToContact);
+
+    }
 }
